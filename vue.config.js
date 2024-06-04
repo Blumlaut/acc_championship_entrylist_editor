2d2f093529ca24e622bc68c3c0e1d5093bd5c3a0
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2024-06-04 17:17:21 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/acc_championship_entrylist_editor/" : "/",
 })
