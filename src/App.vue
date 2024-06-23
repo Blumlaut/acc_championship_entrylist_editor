acf1911da0232ed13894b2c9bfd1c934a3619149
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2024-06-23 19:05:22 +0200

    rewrite in vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,202 @@
-<!-- src/App.vue -->
 <template>
-  <div id="app">
-      <JsonEditor />
-  </div>
+    <v-icon icon="mdi-home" />
+  <v-app id="inspire">
+    <json-editor-drawer
+      :drawer="drawer"
+      :jsonData="jsonData"
+      :selectedCar="selectedCar"
+      :carsArray="carsArray" 
+      @update:drawer="drawer = $event"
+      @select-car="selectCar"
+      @get-car-model="getCarModel"
+      @add-new-car="addNewCar"
+      @delete-car="deleteCar"
+    />
+
+    <v-app-bar app>
+      <v-toolbar-title>ACC Championship Entrylist Editor</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn class="mdi mdi-download titleBarButton" @click="downloadJson" variant="tonal" v-if="jsonData" color="error">Save Entry List</v-btn>
+      <v-btn class="mdi mdi-upload titleBarButton" @click="loadJson" variant="tonal" color="primary">Load Entry List</v-btn>
+      <input type="file" ref="jsonFileInput" @change="onFileChange" accept="text/json" hidden />
+    </v-app-bar>
+
+    <v-main>
+      <json-editor-main
+        :jsonData="jsonData"
+        :selectedCar="selectedCar"
+        :jsonText="jsonText"
+        :carsArray="carsArray"
+        :nationalitiesArray="nationalitiesArray"
+        :cupcategoriesArray="cupcategoriesArray"
+        @load-json="onLoadJson"
+        @add-new-driver="addNewDriver"
+        @remove-driver="removeDriver"
+        @update-json-data="jsonData = $event"
+      />
+    </v-main>
+  </v-app>
 </template>
+
 <script>
-import JsonEditor from './components/jsonEditor.vue';
+import JsonEditorDrawer from './components/JsonEditorDrawer.vue';
+import JsonEditorMain from './components/JsonEditorMain.vue';
+import { cars } from './data/cars.js';
+import { nationalities } from './data/nationalities.js';
+import { cupcategories } from './data/cupcategories.js';
 
 export default {
+  data() {
+    return {
+      drawer: false,
+      jsonData: null,
+      selectedCar: null,
+      jsonText: '',
+      carsArray: [],
+      nationalitiesArray: [],
+      cupcategoriesArray: [],
+    };
+  },
   components: {
-      JsonEditor
-  }
+    JsonEditorDrawer,
+    JsonEditorMain,
+  },
+  created() {
+    this.carsArray = Object.values(cars);
+    this.nationalitiesArray = Object.values(nationalities);
+    this.cupcategoriesArray = Object.values(cupcategories);
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    },
+    loadJson() {
+      this.$refs.jsonFileInput.click();
+    },
+    onFileChange(event) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const jsonstring = e.target.result;
+        this.jsonData = JSON.parse(jsonstring);
+        this.selectedCar = 0;
+        this.jsonText = JSON.stringify(this.jsonData, null, 2);
+      };
+      reader.readAsText(file, 'UTF-16LE');
+    },
+    selectCar(index) {
+      this.selectedCar = index;
+    },
+    getCarModel(carModelType) {
+      return this.carsArray.find(car => car.id === carModelType) || {};
+    },
+    onLoadJson(jsonstring) {
+      this.jsonData = JSON.parse(jsonstring);
+      this.selectedCar = 0;
+      this.jsonText = JSON.stringify(this.jsonData, null, 2);
+    },
+    addNewCar(index) {
+      const newCar = JSON.parse(JSON.stringify(this.jsonData.cars[index]));
+      this.jsonData.cars.push(newCar);
+    },
+    deleteCar(index) {
+      if (this.selectedCar !== null) {
+        this.jsonData.cars.splice(index, 1);
+        this.selectedCar = this.jsonData.cars.length > 0 ? 0 : null;
+      }
+    },
+    addNewDriver(carIndex, newDriver) {
+      this.jsonData.cars[carIndex].drivers.push(newDriver);
+    },
+    removeDriver(carIndex, driverIndex) {
+      if (driverIndex > 0) {
+        this.jsonData.cars[carIndex].drivers.splice(driverIndex, 1);
+      }
+    },
+    downloadJson() {
+      const byteArray = [];
+      byteArray.push(255, 254); // LE BOM
+      for (let i = 0; i < this.jsonText.length; ++i) {
+        const charCode = this.jsonText.charCodeAt(i);
+        byteArray.push(charCode & 0xff);
+        byteArray.push(charCode / 256 >>> 0);
+      }
+      const blob = new Blob([new Uint8Array(byteArray)], { type: 'text/plain;charset=UTF-16LE;' });
+      const blobUrl = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = blobUrl;
+      link.download = 'championship_entrylist.json';
+      link.click();
+    },
+  },
 };
 </script>
 
-<style>
-
-body {
-  max-width: 65% !important
+<style scoped>
+.json-editor {
+  display: flex;
+  flex-direction: column;
 }
 
+.editor-container {
+  display: flex;
+  width: 100%;
+  margin-top: 20px;
+}
+
+.car-list {
+  width: 20%;
+  border-right: 1px solid #ccc;
+  padding-right: 10px;
+}
+
+.car-list ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.car-list li {
+  cursor: pointer;
+  padding: 5px;
+  border-bottom: 1px solid #ccc;
+}
+
+.car-list li:hover {
+  background-color: #f0f0f0;
+}
+
+.car-details {
+  width: 35%;
+  padding-left: 20px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.driver-details {
+  width: 35%;
+  padding-left: 20px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.input-group {
+  margin-bottom: 10px;
+  width: 100%;
+}
+
+.driver-info {
+  margin-top: 20px;
+}
+
+.bold-text {
+  font-weight: bold;
+}
+
+.extra-info {
+  font-size: 12px; /* Adjust the font size as needed */
+}
+
+.titleBarButton {
+  margin-right: 1em;
+}
 </style>
---
+++ src/components/JsonEditorDrawer.vue
@@ -0,0 +1,161 @@
+<template>
+   <v-navigation-drawer v-if="jsonData && jsonData.cars" :value="drawer" app @input="$emit('update:drawer', $event)">
+    <v-list v-if="jsonData && jsonData.cars">
+      <v-list-item
+        v-for="(car, index) in jsonData.cars"
+        :key="index"
+        @click="selectCar(index)"
+        :class="{ 'bold-text': selectedCar === index }"
+        @mouseover="setHoveredItem(index)"
+        @mouseleave="clearHoveredItem()"
+      >
+        <v-list-item-content>
+          <v-list-item-title>
+            #{{ car.info.raceNumber }} {{ car.info.teamName }}
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <span class="extra-info">{{ getCarModel(car.info.carModelType).model }}</span> - 
+            <span class="extra-info">{{ getDriversList(car.drivers) }}</span>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action class="copyCar" @click="addNewCar(index)" v-if="hoveredItem === index">
+          <v-icon color="green">mdi-plus</v-icon>
+        </v-list-item-action>
+        <v-list-item-action class="deleteCar" @click="confirmDeleteCar(index)" v-if="hoveredItem === index">
+          <v-icon color="error">mdi-close</v-icon>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
+    <v-dialog v-model="deleteDialog" max-width="400">
+      <v-card>
+        <v-card-title class="headline">Confirm Deletion</v-card-title>
+        <v-card-text>
+          Are you sure you want to delete this car?
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="error" text @click="deleteCarConfirmed">Yes</v-btn>
+          <v-btn text @click="cancelDeleteCar">Cancel</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  computed: {
+    drawerVisible() {
+      return !!(this.jsonData && this.jsonData.cars);
+    }
+  },
+  props: {
+    drawer: Boolean,
+    jsonData: Object,
+    selectedCar: Number,
+    carsArray: Array,
+  },
+  data() {
+    return {
+      deleteDialog: false,
+      carToDeleteIndex: null,
+      hoveredItem: null,
+      hoveredNewItem: false,
+    };
+  },
+  methods: {
+    selectCar(index) {
+      this.$emit('select-car', index);
+    },
+    getCarModel(carModelType) {
+      return this.carsArray.find(car => car.id === carModelType) || {};
+    },
+    getDriversList(drivers) {
+      if (!drivers) return '';
+      return drivers.map(driver => driver.info.shortName).join(', ');
+    },
+    confirmDeleteCar(index) {
+      this.carToDeleteIndex = index;
+      this.deleteDialog = true;
+    },
+    deleteCarConfirmed() {
+      this.$emit('delete-car', this.carToDeleteIndex);
+      this.closeDeleteDialog();
+    },
+    cancelDeleteCar() {
+      this.closeDeleteDialog();
+    },
+    closeDeleteDialog() {
+      this.deleteDialog = false;
+      this.carToDeleteIndex = null;
+    },
+    setHoveredItem(index) {
+      this.hoveredItem = index;
+    },
+    clearHoveredItem() {
+      this.hoveredItem = null;
+    },
+    setHoveredNewItem(value) {
+      this.hoveredNewItem = value;
+    },
+    clearHoveredNewItem() {
+      this.hoveredNewItem = false;
+    },
+    addNewCar(index) {
+      this.$emit('add-new-car', index);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.bold-text {
+  font-weight: bold;
+  background: #444;
+}
+
+.extra-info {
+  font-size: 12px;
+}
+
+.v-list-item {
+  margin-left: 0px;
+  padding-left: 0px;
+  padding-inline: 0px !important;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  position: relative; /* Ensure positioning context */
+}
+
+.v-list-item-title {
+  margin-left: 25px;
+}
+
+.v-list-item-subtitle {
+  margin-left: 25px;
+}
+
+.v-list-item-action {
+  position: absolute;
+  cursor: pointer;
+  display: none; /* Hide by default */
+}
+
+.copyCar {
+  top:0px
+}
+
+.deleteCar {
+  bottom:0px;
+}
+
+.v-list-item:hover .v-list-item-action,
+.v-list-item:hover .v-list-item-add {
+  display: block; /* Show when parent (v-list-item or v-list-item-add) is hovered */
+}
+
+.navButtons {
+  margin-top: 1em;
+  margin-left: 1px;
+}
+</style>
